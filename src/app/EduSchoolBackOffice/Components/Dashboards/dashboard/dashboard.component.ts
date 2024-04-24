1de01c3c1aaccae771eb181f698456b575d0b415
marwa-wesleti <marwa.wesleti007@gmail/com>
2024-04-24 14:00:27 +0100

    first commit

--- src/app/EduSchoolBackOffice/Components/Dashboards/dashboard/dashboard.component.ts
+++ src/app/EduSchoolBackOffice/Components/Dashboards/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -7,6 +8,20 @@ import { MatSidenav } from '@angular/material/sidenav';
 })
 export class DashboardComponent implements OnInit{
   ngOnInit(): void {
+  }
+  badgeCount:number;
+
+  constructor(
+    private router:Router,
+    ) {
+        this.badgeCount=0;
+
+     }
+
+
+  logout() {
+    localStorage.removeItem("connectedUser");
+    this.router.navigate(['']);
   }
 
 }

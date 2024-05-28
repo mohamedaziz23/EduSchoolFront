fd6eda4a9921bac295a80aff30c1fceec54934a2
awbenhmida <awbenhmida@vermeg.com>
2024-05-28 21:58:17 +0100

    taw

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import {DashboardComponent} from "./EduSchoolBackOffice/Components/Dashboards/dashboard/dashboard.component";
 import {HomePageComponent} from "./EduSchoolFrontOffice/Components/home-page/home-page.component";
 import { DashboardEnseignantComponent } from './EduSchoolBackOffice/Components/Dashboards/dashboard-enseignant/dashboard-enseignant.component';
+import { DashboardEleveComponent } from './EduSchoolBackOffice/Components/Dashboards/dashboard-eleve/dashboard-eleve.component';
+
 import { ListClasseComponent } from './EduSchoolBackOffice/Components/gestionPedagogique/classe/list-classe/list-classe.component';
+import { LoginComponent } from './login/login.component';
+import { UserListComponent } from './EduSchoolBackOffice/Components/Dashboards/user-list/user-list.component';
 
 const routes: Routes = [
   { path: "", component: HomePageComponent },
@@ -13,6 +17,11 @@ const routes: Routes = [
 
   ]},
   {path:"DashboardEnseignant",component:DashboardEnseignantComponent},
+  {path:"DashboardStudent",component:DashboardEleveComponent},
+
+  {path:"Login",component:LoginComponent},
+  {path:"listuser",component:UserListComponent},
+
 
 
 ];

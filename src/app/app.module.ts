c639df81db24c8dc231779af5c8c73064c95d6e4
awbenhmida <awbenhmida@vermeg.com>
2024-07-11 13:49:28 +0100

    nownownowkk

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -33,6 +33,7 @@ import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { CommonModule } from '@angular/common';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 
 @NgModule({
   declarations: [
@@ -50,6 +51,7 @@ import { CommonModule } from '@angular/common';
   imports: [
     BrowserModule,
     HttpClientModule,
+    MatButtonToggleModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
